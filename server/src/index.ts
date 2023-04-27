f605eb9d49453e7468f1744262f157fe38c962c7
h1dd3ngod <nikbog03@gmail.com>
2023-04-27 15:07:34 +0300

    feat: boilerplate socket code for chat app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,51 @@
-const ugabuga = 'hello'
-console.log(ugabuga)
+import express from 'express'
+import cors from 'cors'
+import http from 'http'
+// for socket.io error handling
+import {Server} from 'socket.io'
+// actual socket.io
+
+const app = express();
+
+app.use(cors());
+
+
+
+const PORT = 5000
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`${socket.id} has connected`);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`${socket.id} has joined room ${data}`);
+    });
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+        console.log(
+            `${socket.id} with name ${data.author} has sent a message to room ${data.room}`
+        );
+    });
+    socket.on("disconnect", () => {
+        console.log(`${socket.id} has disconnected`);
+    });
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
